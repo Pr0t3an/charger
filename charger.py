73fa7a9901de27e9e96bd2afe13142e027b841fc
Pr0t3an <admin@pr0t3an.com>
2018-12-28 02:46:08 +0000

    Update charger.py

--- charger.py
+++ charger.py
@@ -18,6 +18,19 @@ except ImportError:
 fields = ['m_unique', 'm_date_utc', 'm_fullheader','m_from','m_replyto','m_to','m_cc','m_subject','m_contenttype','m_importance','m_xmailer', 'm_returnpath', 'm_client','m_id','m_date', 'm_body', 'm_attachment_count']
 temailproccessor = {}
 
+def outputjson(jfilepath, temailproccessor):
+    with open(jfilepath, "w") as f:
+        json.dump(temailproccessor, f, default=str)
+        print "\n[+] Export Complete: JSON file written to {}\n".format(jfilepath)
+
+
+def outputcsv(cfilepath, temailproccessor):
+    with open(cfilepath, "wb") as cpath:
+        wr = unicodecsv.DictWriter(cpath, fields)
+        wr.writeheader()
+        for k in temailproccessor:
+            wr.writerow({field: temailproccessor[k].get(field) or k for field in fields})
+    print "\n[+] Export Complete: CSV file written to {}\n".format(cfilepath)
 
 def main():
     """Run main function."""
@@ -27,24 +40,17 @@ def main():
     parser.add_argument('-j', '--json', help='Output to JSON', action='store_true')
     parser.add_argument('-o', '--output', help='Output Directory')
     args, _ = parser.parse_known_args()
-    print args.output
-    print args.pst
+
     if os.path.isdir(args.output):
         genv = cro.id_generator()
         jfilepath = os.path.join(args.output, "pst_" + genv + ".json")
         cfilepath = os.path.join(args.output, "pst_" + genv + ".csv")
         temailproccessor = cpar.launchstuff(args.pst)
         if args.json:
-            with open(jfilepath, "w") as f:
-                json.dump(temailproccessor, f, default=str)
-                print "\n[+] Export Complete: JSON file written to {}\n".format(jfilepath)
+            outputjson(jfilepath, temailproccessor)
         if args.csv:
-            with open(cfilepath, "wb") as cpath:
-                wr = unicodecsv.DictWriter(cpath, fields)
-                wr.writeheader()
-                for k in temailproccessor:
-                    wr.writerow({field: temailproccessor[k].get(field) or k for field in fields})
-            print "\n[+] Export Complete: CSV file written to {}\n".format(cfilepath)
+            outputcsv(cfilepath, temailproccessor)
+
 
 if __name__ == "__main__":
     ip.important()
